ba1395c76529c402922cdc951ae7f8154f2146fd
natakers <n.kersnovskaya@gmail.com>
2021-08-03 22:48:42 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+	console.log(array);
+	//console.log('njj' + array.length);
+	if (array == undefined) {return 0;}
+	if ((array.length !== 0) && (array !== undefined)) {
+	var m = array[0];
+		for (var i = 0; i < array.length; i++) {
+			if (m > array[i]) {
+				m = array[i];
+			}
+		}
+		return m;
+	}
+	return 0;
 }
 
 exports.max = function max (array) {
+		if (array === undefined) {return 0}
+	if (array.length !== 0) {
+	var m = array[0];
+		for (var i = 0; i < array.length; i++) {
+			if (m < array[i]) {
+				m = array[i];
+			}
+		}
+		return m;
+	}
   return 0;
 }
 
 exports.avg = function avg (array) {
+		if (array === undefined) {return 0}
+		if (array.length !== 0) {
+	var m = 0;
+		for (var i = 0; i < array.length; i++) {
+			m = m+array[i];
+		}
+		return (m/array.length);
+	}
   return 0;
 }
